bd83dddd1e96cc488bcd11254ce20dff1a938f73
Andy Su <andysu1551@gmail.com>
2026-01-24 12:59:51 -0800

    refactor: extract reducer and initial state to separate module
    - Move initialState object to issuesReducer.js
    - Move issuesReducer function to separate module
    - Export both as named exports
    - 47 lines total

---
+++ client/src/contexts/issues/issuesReducer.js
@@ -0,0 +1,47 @@
+export const initialState = {
+  issues: [],
+  allIssues: [],
+  stats: {
+    total: 0,
+    todo: 0,
+    in_progress: 0,
+    review: 0,
+    done: 0,
+  },
+  loading: true,
+  expandedIssues: new Set(),
+  subtasksCache: {},
+};
+
+export function issuesReducer(state, action) {
+  switch (action.type) {
+    case "SET_LOADING":
+      return { ...state, loading: action.value };
+    case "SET_ISSUES":
+      return { ...state, issues: action.value };
+    case "SET_ALL_ISSUES":
+      return { ...state, allIssues: action.value };
+    case "SET_STATS":
+      return { ...state, stats: action.value };
+    case "SET_EXPANDED_ISSUES":
+      return { ...state, expandedIssues: new Set(action.value) };
+    case "SET_SUBTASKS_CACHE":
+      return { ...state, subtasksCache: action.value };
+    case "MERGE_SUBTASKS_CACHE":
+      return {
+        ...state,
+        subtasksCache: { ...state.subtasksCache, ...action.value },
+      };
+    case "UPDATE_ISSUE":
+      return {
+        ...state,
+        issues: state.issues.map((issue) =>
+          issue.id === action.value.id ? action.value : issue,
+        ),
+      };
+    case "ADD_ISSUE":
+      return { ...state, issues: [action.value, ...state.issues] };
+    default:
+      return state;
+  }
+}
