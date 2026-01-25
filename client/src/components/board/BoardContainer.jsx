3779ef0356377cff0d81a49e827a1be92cc555ba
Andy Su <andysu1551@gmail.com>
2026-01-24 20:03:11 -0800

    refactor: simplify BoardContainer by removing FilterPanel

--- client/src/components/board/BoardContainer.jsx
+++ client/src/components/board/BoardContainer.jsx
@@ -1,8 +1,6 @@
 import { Board } from "./Board";
-import { FilterPanel } from "./FilterPanel";
 import { useBoard } from "../../contexts/hooks/useBoard";
 import { useUI } from "../../contexts/UIContext";
-import { useUsers } from "../../contexts/UsersContext";
 
 export function BoardContainer({ isTouchDevice }) {
   const {
@@ -18,40 +16,30 @@ export function BoardContainer({ isTouchDevice }) {
     toggleSubtasks,
     loadMoreIssues,
     requestAddSubtask,
-    filterPanelExpanded,
-    handleFiltersChange,
   } = useBoard();
   const { setCreateStatus, setShowCreateModal, setSelectedIssue } = useUI();
-  const { currentUserId } = useUsers();
 
   return (
-    <>
-      <FilterPanel
-        expanded={filterPanelExpanded}
-        currentUserId={currentUserId}
-        onFiltersChange={handleFiltersChange}
-      />
-      <Board
-        issuesByStatus={issuesByStatus}
-        users={users}
-        onIssueClick={setSelectedIssue}
-        onAddClick={(status) => {
-          setCreateStatus(status);
-          setShowCreateModal(true);
-        }}
-        onDrop={handleStatusChange}
-        onStatusChange={handleStatusChange}
-        onUpdateIssue={updateIssue}
-        onDeleteIssue={deleteIssue}
-        onSubtaskChange={handleSubtaskChange}
-        expandedIssues={expandedIssues}
-        subtasksCache={subtasksCache}
-        onToggleSubtasks={toggleSubtasks}
-        onRequestAddSubtask={requestAddSubtask}
-        isTouchDevice={isTouchDevice}
-        paginationState={paginationState}
-        onLoadMore={loadMoreIssues}
-      />
-    </>
+    <Board
+      issuesByStatus={issuesByStatus}
+      users={users}
+      onIssueClick={setSelectedIssue}
+      onAddClick={(status) => {
+        setCreateStatus(status);
+        setShowCreateModal(true);
+      }}
+      onDrop={handleStatusChange}
+      onStatusChange={handleStatusChange}
+      onUpdateIssue={updateIssue}
+      onDeleteIssue={deleteIssue}
+      onSubtaskChange={handleSubtaskChange}
+      expandedIssues={expandedIssues}
+      subtasksCache={subtasksCache}
+      onToggleSubtasks={toggleSubtasks}
+      onRequestAddSubtask={requestAddSubtask}
+      isTouchDevice={isTouchDevice}
+      paginationState={paginationState}
+      onLoadMore={loadMoreIssues}
+    />
   );
 }
